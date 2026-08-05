a8b1629f8ba3a10db474363f323325b3d7694148
RebornedBrain <RebornedBrain@gmail.com>
2026-08-05 13:50:02 +0300

    New command intercom frame type and command type added internally to engine. Also logic for allocation command frame buffer and conversion added

--- applications/services/ble/ble_command_engine.c
+++ applications/services/ble/ble_command_engine.c
@@ -1,6 +1,24 @@
 #include "ble_command_engine.h"
 #include "ble_log.h"
 
+#define TAG "BleCmdEng"
+
+#include <api_lock.h>
+
+typedef struct {
+    BleSystemCommand command_code;
+    FuriApiLock lock;
+    size_t data_size;
+    void* data;
+    bool result;
+    bool internal;
+} BleCommand;
+
+typedef struct {
+    BleIntercomFrameHeader header;
+    uint8_t data[];
+} BleCommandIntercomFrame;
+
 struct BleCommandEngine {
     uint8_t commands_count;
     const BleCommandItem* commands;
@@ -69,3 +87,30 @@ bool ble_command_engine_run(BleCommandEngine* instance, BleIntercomFrameGeneric*
 
     return result;
 }
+
+static inline void
+    ble_command_frame_check_alloc(BleCommandEngine* instance, const size_t new_frame_size) {
+    if(new_frame_size > instance->frame_size) {
+        instance->frame = realloc(instance->frame, new_frame_size);
+        furi_check(instance->frame);
+        instance->frame_size = new_frame_size;
+    }
+}
+
+static void ble_command_convert_to_intercom_frame(
+    BleCommandEngine* instance,
+    const BleCommand* const command) {
+    const size_t new_msg_size = sizeof(BleIntercomFrameHeader) + command->data_size + sizeof(bool);
+    ble_command_frame_check_alloc(instance, new_msg_size);
+
+    BleCommandIntercomFrame* frame = instance->frame;
+    frame->header.command = command->command_code;
+    frame->header.frame_type = BleIntercomFrameTypeRequest;
+    frame->header.source = BleIntercomFrameSourceSystem;
+    frame->header.data_size = command->data_size;
+
+    if(command->data_size > 0) {
+        memcpy(frame->data, command->data, command->data_size);
+    }
+}
+
